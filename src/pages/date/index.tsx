1db7bddd2f6548109f341183d6a2e3476a43ffca
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2025-05-12 22:51:32 +0900

    fix :: 유저 피드백 반영

--- src/pages/date/index.tsx
+++ src/pages/date/index.tsx
@@ -5,6 +5,8 @@ import Image from 'next/image';
 import ImageWithSkeleton from '../../components/ImageWithSkeleton';
 import Link from 'next/link';
 import styles from './styles.module.scss';
+import { Place } from '../../types';
+import { placesData } from '../../data/places';
 
 // 데이트 아이디어 인터페이스
 interface DateIdea {
@@ -63,8 +65,6 @@ const dateIdeas: DateIdea[] = [
   }
 ];
 
-
-
 // 날씨 예시 데이터 (실제로는 API에서 가져올 예정)
 const weatherExample = {
   main: {
@@ -79,412 +79,20 @@ const weatherExample = {
   ]
 };
 
-interface Place {
-  id: number;
-  name: string;
-  lat: number;
-  lng: number;
-  description: string;
-  rating: number;
-  tags: string[];
-  mainImage: string;
-  images: string[]; // 추가 이미지들 (모달에서 보여질 이미지들)
-  naver: string; // 네이버 지도 링크
-}
-
-// 예시 장소 데이터
-const placesData: Place[] = [
-  {
-    id: 4,
-    name: "본지르르 성수",
-    lat: 37.549347883,
-    lng: 127.054658034,
-    description: "성수동에서 작업하기 좋은 카페, 널찍한 자리와 조용한 분위기",
-    rating: 4.6,
-    tags: ["카페", "성수동", "조용한", "넓은공간", "작업하기좋은"],
-    mainImage: "/sungsu_vonzurr1.png",
-    images: [
-      "/sungsu_vonzurr1.png",
-      "/sungsu_vonzurr2.png",
-      "/sungsu_vonzurr3.png",
-    ],
-    naver: "https://naver.me/xdpFyokt"
-  },
-  {
-    id: 5,
-    name: "포레스트 서울숲",
-    lat: 37.546801119,
-    lng: 127.04073112,
-    description:
-      "서울숲 초근접, 빙수와 커피, 널찍한 자리 카공하기도 좋고 빈백에 누워서 쉬기도 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "서울숲", "빙수", "빈백", "휴식공간"],
-    mainImage: "/forest_seoul_1.png",
-    images: [
-      "/forest_seoul_1.png",
-      "/forest_seoul_2.png",
-      "/forest_seoul_3.png",
-      "/forest_seoul_4.png",
-    ],
-    naver: "https://naver.me/FLyT1IsO"
-  },
-  {
-    id: 6,
-    name: "오베뉴 한남",
-    lat: 37.53519945,
-    lng: 127.010243539,
-    description:
-      "통창 테라스, 날씨 좋은 날 가기 좋은 카페, 널찍한 자리와 카공하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "한남동", "테라스", "통창", "뷰맛집"],
-    mainImage: "/ovenu_hannam_1.png",
-    images: [
-      "/ovenu_hannam_1.png",
-      "/ovenu_hannam_2.png",
-      "/ovenu_hannam_3.png",
-      "/ovenu_hannam_4.png",
-      "/ovenu_hannam_5.png",
-    ],
-    naver: "https://naver.me/GALRg97X"
-  },
-  {
-    id: 7,
-    name: "로우커피스탠드 뚝섬",
-    lat: 37.54713165462201,
-    lng: 127.0466312899477,
-    description: "테이크아웃 전문점, 바닐라 라떼가 맛있는 곳, 저렴한 커피 가격",
-    rating: 4.5,
-    tags: ["커피", "테이크아웃", "바닐라라떼", "가성비", "뚝섬"],
-    mainImage: "/low_stand1.png",
-    images: ["/low_stand1.png", "/low_stand_2.png"],
-    naver: "https://naver.me/GeUR9SqN"
-  },
-  {
-    id: 8,
-    name: "쇼트브레드 의정부 민락",
-    lat: 37.746799244,
-    lng: 127.099873165,
-    description: "빵이 맛있는 곳, 널찍한 공간, 카공하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "베이커리", "의정부", "넓은공간", "작업하기좋은"],
-    mainImage: "/shorts_bread_1.png",
-    images: ["/shorts_bread_1.png", "/shorts_bread_2.png"],
-    naver: "https://naver.me/5k7fJRrj"
-  },
-  {
-    id: 9,
-    name: "파블로앤던커피 의정부 민락",
-    lat: 37.74654569229999,
-    lng: 127.09553664518879,
-    description:
-      "식사와 베이커리, 카페를 모두 즐길 수 있는 곳 널찍한 자리 카공하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "베이커리", "식사", "의정부", "넓은공간"],
-    mainImage: "/pablo_1.png",
-    images: ["/pablo_1.png", "/pablo_2.png"],
-    naver: "https://naver.me/xxY2zE8q"
-  },
-  {
-    id: 10,
-    name: "카페 빔프 의정부 민락",
-    lat: 37.746467197,
-    lng: 127.096325224,
-    description:
-      "댕댕이가 있는 카페, 장난스러운 소품들이 가득한 카공하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "반려동물", "소품샵", "의정부", "아기자기한"],
-    mainImage: "/bimp_1.png",
-    images: ["/bimp_1.png", "/bimp_2.jpg"],
-    naver: "https://naver.me/GV29Wmze"
-  },
-  {
-    id: 11,
-    name: "나루터 송리단길",
-    lat: 37.74651225,
-    lng: 127.095550362,
-    description: "송리단길 초근접, 널찍한 자리 카공하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "송리단길", "넓은공간", "작업하기좋은", "핫플레이스"],
-    mainImage: "/narut_1.png",
-    images: ["/narut_1.png", "/narut_2.png"],
-    naver: "https://naver.me/F1rxlzl9"
-  },
-  {
-    id: 12,
-    name: "씨엘트리 송리단길",
-    lat: 37.509559963,
-    lng: 127.109418759,
-    description: "뷰가 좋고, 커피가 맛있는 카공하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "뷰맛집", "맛있는커피", "송리단길", "작업하기좋은"],
-    mainImage: "/cltree_3.png",
-    images: ["/cltree_1.png", "/cltree_2.png", "/cltree_3.png"],
-    naver: "https://naver.me/x8tpEpVWㅂ"
-  },
-  {
-    id: 13,
-    name: "바이러닉 에스프레소 바 성수점",
-    lat: 37.54633912,
-    lng: 127.0490827,
-    description: "모던한 분위기에서 커피를 즐길 수 있는 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "성수동", "모던한", "에스프레소", "작업하기좋은"],
-    mainImage: "/byrunic_1.png",
-    images: ["/byrunic_1.png", "/byrunic_2.png"],
-    naver: "https://naver.me/Fafyi8Gq"
-  },
-  {
-    id: 14,
-    name: "어라운드 데이 서울숲",
-    lat: 37.546858678,
-    lng: 127.041259584,
-    description:
-      "가정집을 개조해 화이트톤의 개방감 있는 분위기의 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "서울숲", "화이트톤", "개방감", "리모델링"],
-    mainImage: "/around_day_1.png",
-    images: [
-      "/around_day_1.png",
-      "/around_day_3.png"
-    ],
-    naver: "https://naver.me/57VZrubZ"
-  },
-  
-  {
-    id: 15,
-    name: "카페 할아버지공장 성수",
-    lat: 37.540993413,
-    lng: 127.055242863,
-    description:
-      "맛있는 빵과 커피를 즐기며 큰 실내외공간에서 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "베이커리", "성수동", "넓은공간", "실내외공간"],
-    mainImage: "/grandpa_1.png",
-    images: ["/grandpa_1.png", "/grandpa_2.png", "/grandpa_3.png"],
-    naver: "https://naver.me/GDa2dyWW"
-  },
-  {
-    id: 16,
-    name: "코히루 송리단길",
-    lat: 37.510480019,
-    lng: 127.054825429,
-    description:
-      "일본 분위기의 아기자기한 분위기와 커피가 맛있는 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "일본감성", "아기자기한", "송리단길", "맛있는커피"],
-    mainImage: "/koheru_1.png",
-    images: ["/koheru_1.png", "/koheru_2.png", "/koheru_3.png"],
-    naver: "https://naver.me/5chuGGiR"
-  },
-  {
-    id: 17,
-    name: "카페 사브레 송리단길",
-    lat: 37.546858678,
-    lng: 127.041259584,
-    description: "가정집을 개조한 인테리어로 데이트 및 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "리모델링", "데이트", "송리단길", "작업하기좋은"],
-    mainImage: "/sab_1.jpg",
-    images: ["/sab_1.jpg", "/sab_2.jpg", "/sab_3.jpg"],
-    naver: "https://naver.me/G0DXUJNn"
-  },
-  {
-    id: 18,
-    name: "더 노벰버 라운지 하남풍산점",
-    lat: 37.555195733,
-    lng: 127.20571837,
-    description: "24시간 넓은 통창 뷰와 좌석, 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "24시간", "통창", "하남", "뷰맛집"],
-    mainImage: "/hanam_1.png",
-    images: ["/hanam_1.png", "/hanam_2.png"],
-    naver: "https://naver.me/G7V4GsMu"
-  },
-  {
-    id: 19,
-    name: "오브제 커피 하남미사",
-    lat: 37.561434896,
-    lng: 127.191185701,
-    description: "늦게까지 하는 아늑한 분위기에 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "늦은영업", "아늑한", "하남", "작업하기좋은"],
-    mainImage: "/object_1.png",
-    images: ["/object_1.png", "/object_2.png", "/object_3.png"],
-    naver: "https://naver.me/FEUpqHi5"
-  },
-  {
-    id: 20,
-    name: "뜨아아커피집 왕십리",
-    lat: 37.560300966,
-    lng: 127.035102353,
-    description: "고즈넉한 한옥 인테리어에서 즐기는 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "한옥", "고즈넉한", "왕십리", "작업하기좋은"],
-    mainImage: "/ddaaa_1.png",
-    images: ["/ddaaa_1.png", "/ddaaa_2.png", "/ddaaa_3.png"],
-    naver: "https://naver.me/G4WoWwaS"
-  },
-  {
-    id: 21,
-    name: "갈십리 왕십리",
-    lat:37.558925815,
-    lng: 127.040447249,
-    description: "귀여운 고양이와 일본 빈티지 감성으로 채워진 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "고양이", "일본감성", "빈티지", "왕십리"],
-    mainImage: "/gal_1.jpg",
-    images: ["/gal_1.jpg", "/gal_2.jpg", "/gal_3.jpg"],
-    naver: "https://naver.me/GZZuXi2n"
-  },
-  {
-    id: 22,
-    name: "어질인 왕십리",
-    lat:37.56136111366082,
-    lng: 127.03954505441635,
-    description: "맛있는 디저트와 아기자기한 인테리어와 함께 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "디저트", "아기자기한", "왕십리", "작업하기좋은"],
-    mainImage: "/in_1.png",
-    images: ["/in_1.png", "/in_2.png", "/in_3.png"],
-    naver: "https://naver.me/GPdoMFSO"
-  },
-  {
-    id: 23,
-    name: "퀜치 커피 합정",
-    lat:37.55401268388709,
-    lng: 126.91284725933576,
-    description: "모던한 우드톤 분위기에 커피가 정말 맛있는 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "우드톤", "모던한", "맛있는커피", "합정"],
-    mainImage: "/quench_1.png",
-    images: ["/quench_1.png", "/quench_2.png", "/quench_3.png"],
-    naver: "https://naver.me/FK5vi2LB"
-  },
-  {
-    id: 24,
-    name: "노띵크 연남",
-    lat:37.56196974001513,
-    lng: 126.92695792449092,
-    description: "편안한 분위기과 커피가 맛있는 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "편안한", "맛있는커피", "연남", "작업하기좋은"],
-    mainImage: "/nothink_1.png",
-    images: ["/nothink_1.png", "/nothink_2.png", "/nothink_3.png"],
-    naver: "https://naver.me/5PVaUjN3"
-  },
-  {
-    id: 25,
-    name: "나이스워크투데이 연남",
-    lat:37.54823636363809,
-    lng: 126.9397714783147,
-    description: "쾌적한 분위기와 카페 이름에 걸맞게 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "쾌적한", "작업하기좋은", "연남", "워크스페이스"],
-    mainImage: "/nice_1.png",
-    images: ["/nice_1.png", "/nice_2.png", "/nice_3.png"],
-    naver: "https://naver.me/FV7Ykqcl"
-  },
-  {
-    id: 26,
-    name: "메모러블모먼트 둔촌",
-    lat:37.523490661,
-    lng: 127.061846472,
-    description: "세련되고 블랙톤 인테리어가 멋진, 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "블랙톤", "세련된", "둔촌동", "작업하기좋은"],
-    mainImage: "/dunch_1.png",
-    images: ["/dunch_1.png"],
-    naver: "https://naver.me/GV2t5ViM"
-  },
-  {
-    id: 27,
-    name: "VGG 송리단길",
-    lat:37.51116294020035,
-    lng: 127.10742285324267,
-    description: "주황색 톤의 모던한 인테리어가 멋진, 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "오렌지톤", "모던한", "송리단길", "작업하기좋은"],
-    mainImage: "/vgg_1.png",
-    images: ["/vgg_1.png", "/vgg_2.png"],
-    naver: "https://naver.me/xiquO1WL"
-  },
-  {
-    id: 28,
-    name: "텟어텟 봉은사",
-    lat:37.513508392,
-    lng:127.061846472,
-    description: "베이커리가 맛있는 코엑스 근처 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "베이커리", "코엑스", "봉은사", "작업하기좋은"],
-    mainImage: "/tat_1.png",
-    images: ["/tat_1.png", "/tat_2.png", "/tat_3.png"],
-    naver: "https://naver.me/FQVGAyDg"
-  },
-  {
-    id: 29,
-    name: "누누커피 코엑스",
-    lat:37.51259679590774,
-    lng:127.05882787833228,
-    description: "누누커피가 절대적으로 맛있는 코엑스 내부 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "맛있는커피", "코엑스", "실내", "작업하기좋은"],
-    mainImage: "/in_1.png",
-    images: ["/in_1.png", "/in_2.png", "/in_3.png"],
-    naver: "https://naver.me/I55a9Y0s"
-  },
-  {
-    id: 30,
-    name: "한남 작업실",
-    lat:37.536527911096,
-    lng:126.99876064931556,
-    description: "정원 인테리어와 커피가 정말 맛있는 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "정원", "맛있는커피", "한남동", "작업하기좋은"],
-    mainImage: "/hannam_work_1.png",
-    images: ["/hannam_work_1.png", "/hannam_work_2.png", "/hannam_work_3.png"],
-    naver: "https://naver.me/Fio7BqKq"
-  },
-  {
-    id: 31,
-    name: "mtl 한남",
-    lat:37.53714098665077,
-    lng:126.99904436530372,
-    description: "모던한 우드톤, 넓은 통창에다가 베이커리가 맛있는 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "우드톤", "통창", "한남동", "베이커리"],
-    mainImage: "/mtl_1.png",
-    images: ["/mtl_1.png", "/mtl_2.png", "/mtl_3.png"],
-    naver: "https://naver.me/xoH8AtfT"
-  },
-  {
-    id: 32,
-    name: "맥심 플랜트 한남",
-    lat:37.536953564,
-    lng:127.001008565,
-    description: "다양한 커피 종류를 맛볼 수 있고, 4층까지의 넓은 자석으로 작업하기 좋은 카페",
-    rating: 4.5,
-    tags: ["카페", "다양한커피", "멀티층", "한남동", "작업하기좋은"],
-    mainImage: "/maxim_1.png",
-    images: ["/maxim_1.png", "/maxim_2.png", "/maxim_3.png"],
-    naver: "https://naver.me/IFgdNOZo"
-  },
-];
-
 export default function DateIdeasPage() {
   const [filter, setFilter] = useState<'all' | 'indoor' | 'outdoor'>('all');
   const [showModal, setShowModal] = useState(false);
   const [modalIdea, setModalIdea] = useState<DateIdea | null>(null);
   const [modalPlace, setModalPlace] = useState<Place | null>(null);
   
-  // 랜덤 인기 카페 5개 선택
-  const getRandomTopCafes = (cafes: Place[], count: number) => {
-    const shuffled = [...cafes].sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, count);
-  };
+  const topCafes = [
+    placesData.find(cafe => cafe.name === "본지르르 성수"),
+    placesData.find(cafe => cafe.name === "퀜치 커피 합정"),
+    placesData.find(cafe => cafe.name === "오베뉴 한남"),
+    placesData.find(cafe => cafe.name === "mtl 한남"),
+    placesData.find(cafe => cafe.name === "씨엘트리 송리단길")
+  ].filter(cafe => cafe !== undefined) as Place[];
   
-  const topCafes = getRandomTopCafes(placesData, 5);
-  
-  // 카페를 지역별로 그룹화
   const groupCafesByRegion = (cafes: Place[]) => {
     const regions: { [key: string]: Place[] } = {
       '성수': [],
@@ -517,7 +125,7 @@ export default function DateIdeasPage() {
         regions['한남/이태원'].push(cafe);
       } else if (cafe.tags.includes('송리단길')) {
         regions['잠실/송리단길'].push(cafe);
-      } else if (cafe.tags.includes('합정') || cafe.tags.includes('연남')) {
+      } else if (cafe.tags.includes('합정') || cafe.tags.includes('연남동')) {
         regions['합정/연남'].push(cafe);
       } else if (cafe.tags.includes('봉은사') || cafe.tags.includes('코엑스')) {
         regions['봉은사/코엑스'].push(cafe);
@@ -526,7 +134,6 @@ export default function DateIdeasPage() {
       }
     });
     
-    // 빈 지역 필터링
     return Object.fromEntries(
       Object.entries(regions).filter(([_, cafes]) => cafes.length > 0)
     );
@@ -554,7 +161,6 @@ export default function DateIdeasPage() {
       </div>
       
       <div className={styles.main}>
-        {/* Top 5 인기 카페 */}
         <div className={styles.popularSection}>
           <h2 className={styles.sectionTitle}>지금 뜨고 있는 인기 카페 TOP 5</h2>
           <div className={styles.popularCafes}>
@@ -582,7 +188,6 @@ export default function DateIdeasPage() {
           </div>
         </div>
         
-        {/* 지역별 카페 */}
         <div className={styles.regionSection}>
           <h2 className={styles.sectionTitle}>지역별 인기 카페</h2>
           
@@ -622,7 +227,6 @@ export default function DateIdeasPage() {
         </div>
       </div>
       
-      {/* 카페 상세 정보 모달 */}
       {showModal && modalPlace && (
         <div className={styles.modalOverlay} onClick={closeModal}>
           <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export interface Place {
+  id: number;
+  name: string;
+  lat: number;
+  lng: number;
+  description: string;
+  rating: number;
+  tags: string[];
+  mainImage: string;
+  images: string[];
+  naver: string;
+} 
